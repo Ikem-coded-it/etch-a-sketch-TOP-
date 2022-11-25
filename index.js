1c5531f6429ccc0760102c6c2cb791573597eea0
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2022-11-25 22:04:35 +0100

    completed project

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const createGrid = () => {
     const container = document.querySelector('.container');
     for(let i = 0; i < 256; i++) {
-        let gridDivs = document.createElement('div');
-        gridDivs.className = 'grid-div';
-        container.appendChild(gridDivs);
+        let gridDiv = document.createElement('div');
+        gridDiv.className = 'grid-div-brown';
+        container.appendChild(gridDiv);
     }
 }
-
 createGrid();
+
+const changeColor = () => {
+    const gridDivs = document.querySelectorAll('.grid-div-brown');
+    gridDivs.forEach(div => {
+        div.addEventListener('click', () => {
+            div.classList.toggle('grid-div-white');
+        })
+    })
+}
+
+changeColor()
